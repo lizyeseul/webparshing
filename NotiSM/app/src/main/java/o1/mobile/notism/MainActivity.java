28999a274bf4602d3a6c31f87f2b226bae7cd87d
ys <lys984prms@naver.com>
2019-06-20 23:42:20 +0900

    ver1.01
    jsoup구성중

--- NotiSM/app/src/main/java/o1/mobile/notism/MainActivity.java
+++ NotiSM/app/src/main/java/o1/mobile/notism/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     Elements contents;
     Document doc = null;
     String Top10="null";
+    String[] items = new String[]{"번호","말머리","제목","이름","날짜","조회수"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,22 +43,27 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     protected Object doInBackground(Object[] params) {
                         try {
-                            doc = Jsoup.connect("http://www.naver.com").get();
-                            contents = doc.select("ul.ah_l > li > a[href=#]");
-                        } catch (IOException e) {
+                            //doc = Jsoup.connect("http://exo.smtown.com/Board/List/10724").get();
+                            doc = Jsoup.connect("http://exo.smtown.com/Board/List/?kind=1").get();
+                            //doc = Jsoup.connect("http://www.naver.com").get();
+                            contents = doc.select("div.wrap > div");
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
 
 
                         Top10 = "";
-                        int count = 0;
+                        Top10 += contents.size() + "\n";
+                        Log.d("logg1","top: "+Top10);
                         for (Element element : contents) {
-                            count++;
-                            Top10 += count + ". " + element.select(".ah_k").text() + "\n";
-
-                            if (count == 10)
-                                break;
+                            Log.d("logg2","top: "+Top10);
+                            /*Iterator<Element> iterElem = element.getElementsByTag("td").iterator();
+                            for(String item : items){
+                                Log.d("logg3","top: "+Top10);
+                                Top10 += item+": "+iterElem.next().text() + "\n";
+                            }*/
                         }
+                        Log.d("logg4","top: "+Top10);
                         return null;
                     }
 
