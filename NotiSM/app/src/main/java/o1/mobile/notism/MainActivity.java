5b88a938d7123239f854ea4c0449778427488067
ys <lys984prms@naver.com>
2019-06-21 16:28:10 +0900

    ver1.02
    왜 안돼ㅐㅐㅐ
    주소 입력하면 에러창이 불러와짐 인코딩 문제인지 보고 해결하려는 중

--- NotiSM/app/src/main/java/o1/mobile/notism/MainActivity.java
+++ NotiSM/app/src/main/java/o1/mobile/notism/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    WebView browser;
     Elements contents;
     Document doc = null;
     String Top10="null";
@@ -35,26 +38,40 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.text);
         button = findViewById(R.id.button);
+        /*browser = findViewById(R.id.webview);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setWebViewClient(new WebViewClient());
+        browser.loadUrl("http://exo.smtown.com/Board/List/?kind=1");*/
+
 
         button.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("StaticFieldLeak")
             @Override
             public void onClick(View v){
-                new AsyncTask() {
+
+
+               new AsyncTask() {
                     @Override
                     protected Object doInBackground(Object[] params) {
                         try {
                             //doc = Jsoup.connect("http://exo.smtown.com/Board/List/10724").get();
-                            doc = Jsoup.connect("http://exo.smtown.com/Board/List/?kind=1").get();
+                            doc = Jsoup.connect("http://exo.smtown.com/Board/List/?kind=1").timeout(30000)
+                                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                                    .referrer("http://www.google.com").get();
+                            //doc = Jsoup.connect("http%3A%2F%2Fexo.smtown.com%2FBoard%2FList%2F10724").get();
+                            //doc = Jsoup.connect("http://exo.smtown.com/Main").get();
                             //doc = Jsoup.connect("http://www.naver.com").get();
-                            contents = doc.select("div.wrap > div");
+                            contents = doc.select("div");
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
 
-
+                        String temp = contents.text();
+                        Log.d("logg0","temp: "+temp);
                         Top10 = "";
                         Top10 += contents.size() + "\n";
                         Log.d("logg1","top: "+Top10);
+
                         for (Element element : contents) {
                             Log.d("logg2","top: "+Top10);
                             /*Iterator<Element> iterElem = element.getElementsByTag("td").iterator();
